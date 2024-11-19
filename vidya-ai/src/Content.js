bbaf74031cb7f503e4d845253fb5ad3c64bc586b
Armaan <thearmaandeep@gmail.com>
2024-11-19 16:28:54 +0000

    Started project. Just made template

---
+++ vidya-ai/src/Content.js
@@ -0,0 +1,17 @@
+
+
+function Content() {
+    return (
+      <div className="Content">
+         <ul>
+          <li>Home</li>
+          <li>Talk to PDF</li>
+          <li>Roadmap</li>
+          <li>Study</li>
+         </ul>
+      </div>
+    );
+  }
+  
+  export default Content;
+  
---
+++ vidya-ai/src/Sidebar.js
@@ -0,0 +1,16 @@
+
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+       <ul>
+        <li>Home</li>
+        <li>Talk to PDF</li>
+        <li>Roadmap</li>
+        <li>Study</li>
+       </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
